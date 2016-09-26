c911efa94af5aeeb1d21c98c52e960e4002a6f80
Bolstad <geir.bolstad@nina.no>
2016-09-26 13:44:15 +0200

    3SW life history implemented

--- src/popdyn.cpp
+++ src/popdyn.cpp
@@ -6,13 +6,13 @@ Type objective_function<Type>::operator() ()
   // DATA
   DATA_INTEGER(n_t);    //maybe change this to start year and end year?
   DATA_INTEGER(n_pop);
-  int n_class = 2;      //number of sea age categories in the model
+  int n_class = 3;      //number of sea age categories in the model
 
   // Latent variables:
   PARAMETER_ARRAY(lnN_ad); //Number of adult female spawners
 
   // Flags
-  DATA_VECTOR(life_history);
+  DATA_IVECTOR(life_history);
 
 
   // Variables
@@ -38,7 +38,6 @@ Type objective_function<Type>::operator() ()
   DATA_ARRAY(ln_mean_kg_VR_data);    // Data array of dimensions n_years x n_populations x n_weight_class
   DATA_ARRAY(Nfem_catched_VR_data);  // !!!!! change this to log proporiton catched !!!!!!
 
-  // not needed: int n_class_data = 3;      //number of sea age categories in the data
 
   // Priors for initial population sizes
   for(i=0;i<n_pop;i++){
@@ -47,7 +46,12 @@ Type objective_function<Type>::operator() ()
       jnll -= dnorm(lnN_ad(0,i,1), lnNfem_VR_data(0, i, 1), lnNfem_SE_VR_data(0,i,1)/Type(100.0), true);
       jnll -= dnorm(lnN_ad(1,i,1), lnNfem_VR_data(1, i, 1), lnNfem_SE_VR_data(1,i,1)/Type(100.0), true);
     }
-  }
+    if(life_history(i) == 3){
+      jnll -= dnorm(lnN_ad(0,i,2), lnNfem_VR_data(0, i, 2), lnNfem_SE_VR_data(0,i,2)/Type(100.0), true);
+      jnll -= dnorm(lnN_ad(1,i,2), lnNfem_VR_data(1, i, 2), lnNfem_SE_VR_data(1,i,2)/Type(100.0), true);
+      jnll -= dnorm(lnN_ad(2,i,2), lnNfem_VR_data(2, i, 2), lnNfem_SE_VR_data(2,i,2)/Type(100.0), true);
+    }
+    }
 
   // Observation model for Nfem
   for(t=1;t<n_t;t++){
@@ -55,12 +59,17 @@ Type objective_function<Type>::operator() ()
         jnll -= dnorm(lnNfem_VR_data(t, i, 0), lnN_ad(t,i,0), lnNfem_SE_VR_data(t,i,0), true);
       }}
 
-    for(t=2;t<n_t;t++){
-      for(i=0;i<n_pop;i++){
-        if(life_history(i) != 1){
-          jnll -= dnorm(lnNfem_VR_data(t, i, 1), lnN_ad(t,i,1), lnNfem_SE_VR_data(t,i,1), true);
-        }}}
+  for(t=2;t<n_t;t++){
+    for(i=0;i<n_pop;i++){
+      if(life_history(i) != 1){
+        jnll -= dnorm(lnNfem_VR_data(t, i, 1), lnN_ad(t,i,1), lnNfem_SE_VR_data(t,i,1), true);
+      }}}
 
+  for(t=3;t<n_t;t++){
+    for(i=0;i<n_pop;i++){
+      if(life_history(i) == 3){
+        jnll -= dnorm(lnNfem_VR_data(t, i, 2), lnN_ad(t,i,2), lnNfem_SE_VR_data(t,i,2), true);
+      }}}
 
 
   // Observation model for mean_kg
@@ -73,9 +82,12 @@ Type objective_function<Type>::operator() ()
   N_egg.fill(0.0);
   for(t=0;t<n_t;t++){
     for(i=0;i<n_pop;i++){
-      N_egg(t,i) += exp(ln_mean_kg_VR_data(t,i,0)+log(Type(1450.0))+lnN_ad(t,i,0));
+        N_egg(t,i) += exp(ln_mean_kg_VR_data(t,i,0)+log(Type(1450.0))+lnN_ad(t,i,0));
       if(life_history(i) != 1){
         N_egg(t,i) += exp(ln_mean_kg_VR_data(t,i,1)+log(Type(1450.0))+lnN_ad(t,i,1));
+      }
+      if(life_history(i) == 3){
+        N_egg(t,i) += exp(ln_mean_kg_VR_data(t,i,2)+log(Type(1450.0))+lnN_ad(t,i,2));
       }
       //N_egg(t,i) += mean_kg_observed(t,i,0)*Type(1450.0)*(Type(1.0)*N_ad(t,i,0)+Type(0.0)*N_ad(t,i,1));
       //N_egg(t,i) += mean_kg_observed(t,i,1)*Type(1450.0)*(Type(0.0)*N_ad(t,i,0)+Type(1.0)*N_ad(t,i,1));
@@ -91,7 +103,8 @@ Type objective_function<Type>::operator() ()
   PARAMETER(lnP_egg_riv_mean);        //Type lnP_egg_riv_mean   = -exp(-clogP_egg_riv_mean); //to avoid probabilities above 1
   PARAMETER(lnP_riv_riv_mean);        //Type P_riv_riv_mean   = exp(logitP_riv_riv_mean)/(1+exp(logitP_riv_riv_mean));
   PARAMETER(lnP_riv_ad1_mean);        //Type P_sea1_ad_mean   = exp(logitP_sea1_ad_mean)/(1+exp(logitP_sea1_ad_mean));
-  PARAMETER(lnP_riv_ad2_mean);        //Type P_sea1_sea2_mean = exp(logitP_sea1_sea2_mean)/(1+exp(logitP_sea1_sea2_mean));
+  PARAMETER(lnP_riv_ad2_mean);
+  PARAMETER(lnP_riv_ad3_mean);
 
   // Fixed effects (variances)
     // residual
@@ -102,6 +115,7 @@ Type objective_function<Type>::operator() ()
     // pop
     PARAMETER(lnSD_lnP_riv_ad1_pop);
     PARAMETER(lnSD_lnP_riv_ad2_pop);
+    //PARAMETER(lnSD_lnP_riv_ad3_pop);
 
   // Random effects
     // time
@@ -110,6 +124,7 @@ Type objective_function<Type>::operator() ()
     // pop
     PARAMETER_VECTOR(lnP_riv_ad1_pop);
     PARAMETER_VECTOR(lnP_riv_ad2_pop);
+    //PARAMETER_VECTOR(lnP_riv_ad3_pop);
 
 
   // Starting values
@@ -130,6 +145,7 @@ Type objective_function<Type>::operator() ()
   matrix<Type> lnP_riv_sea1(n_t-1, n_pop);
   matrix<Type> lnP_riv_ad1(n_t-1, n_pop);
   matrix<Type> lnP_riv_ad2(n_t-2, n_pop);
+  matrix<Type> lnP_riv_ad3(n_t-3, n_pop);
 
 
 
@@ -152,6 +168,9 @@ Type objective_function<Type>::operator() ()
       if(t>1 && life_history(i) != 1){
         lnP_riv_ad2(t-2, i) = lnP_riv_ad2_mean + lnP_riv_ad2_pop(i) + lnP_riv_sea1_t(t-2) + lnP_sea1_sea2_t(t-2);
       }
+      if(t>2 && life_history(i) == 3){
+        lnP_riv_ad3(t-3, i) = lnP_riv_ad3_mean;// + lnP_riv_ad3_pop(i);// + lnP_riv_sea1_t(t-3) + lnP_sea1_sea2_t(t-3)*Type(2.0);
+      }
 
       // River
       lnN_egg_surviving(t-1, i) = lnN_egg(t-1,i) + lnP_egg_riv(t-1,i);
@@ -166,6 +185,12 @@ Type objective_function<Type>::operator() ()
       if(t>1 && life_history(i) != 1){
       expectation = lnN_riv(t-2,i) + lnP_riv_ad2(t-2,i);
       jnll -= dnorm(lnN_ad(t,i,1), expectation, exp(lnSD_lnN_add_residual), true);
+      }
+
+      // Adults returning sea age 3
+      if(t>2 && life_history(i) == 3){
+        expectation = lnN_riv(t-3,i) + lnP_riv_ad3(t-3,i);
+        jnll -= dnorm(lnN_ad(t,i,2), expectation, exp(lnSD_lnN_add_residual), true);
       }
 
     }}
@@ -183,9 +208,10 @@ Type objective_function<Type>::operator() ()
     if(life_history(i) != 1){
       jnll -= dnorm(lnP_riv_ad2_pop(i), Type(0.0), exp(lnSD_lnP_riv_ad2_pop), true);
     }
+    // if(life_history(i) == 3){
+    //   jnll -= dnorm(lnP_riv_ad3_pop(i), Type(0.0), exp(lnSD_lnP_riv_ad3_pop), true);
+    // }
   }
-
-
 
 
 
